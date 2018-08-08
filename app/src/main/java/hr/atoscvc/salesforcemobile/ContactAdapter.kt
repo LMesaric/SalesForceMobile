594bd42a795b7a75f79c4273ffcff8cba74ec89e
LMesaric <luka.mesaric@fer.hr>
2018-08-08 12:24:09 +0200

    Partially implemented ContactListActivity. No animations or Select buttons.

---
+++ app/src/main/java/hr/atoscvc/salesforcemobile/ContactAdapter.kt
@@ -0,0 +1,66 @@
+package hr.atoscvc.salesforcemobile
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.list_layout_contacts.view.*
+
+class ContactAdapter(private val contactList: ArrayList<Contact>, val context: Context) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+
+    private var currentPosition = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_layout_contacts, parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = contactList[position]
+        val tvCardContactsNameText = "${context.resources.getStringArray(R.array.contactTitle_array)[contact.title]} ${contact.firstName} ${contact.lastName}"
+        holder.tvCardContactsName.text = tvCardContactsNameText
+        holder.tvCardContactCompany.text = contact.company
+        holder.tvCardContactsStatus.text = context.resources.getStringArray(R.array.contactStatus_array)[contact.status]
+        holder.tvCardContactsPhone.text = contact.phone
+        holder.tvCardContactsEmail.text = contact.email
+        holder.tvCardContactsDetails.text = contact.details
+
+        if (currentPosition == position) {
+            if (holder.constraintLayoutContactsExpandable.visibility == View.GONE) {
+//                val slideDown = AnimationUtils.loadAnimation(context, R.anim.animation_cardview_expand)
+                holder.constraintLayoutContactsExpandable.visibility = View.VISIBLE
+//                holder.constraintLayoutContactsExpandable.startAnimation(slideDown)
+            } else {
+//                val slideUp = AnimationUtils.loadAnimation(context, R.anim.animation_cardview_collapse)
+                holder.constraintLayoutContactsExpandable.visibility = View.GONE
+//                holder.constraintLayoutContactsExpandable.startAnimation(slideUp)
+            }
+        }
+
+        holder.constraintLayoutContactsMain.setOnClickListener {
+            currentPosition = holder.adapterPosition
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return contactList.size
+    }
+
+    class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvCardContactsName: TextView = itemView.tvCardContactsName
+        val tvCardContactCompany: TextView = itemView.tvCardContactCompany
+
+        val tvCardContactsStatus: TextView = itemView.tvCardContactsStatus
+        val tvCardContactsPhone: TextView = itemView.tvCardContactsPhone
+        val tvCardContactsEmail: TextView = itemView.tvCardContactsEmail
+        val tvCardContactsDetails: TextView = itemView.tvCardContactsDetails
+
+        val constraintLayoutContactsMain: ConstraintLayout = itemView.constraintLayoutContactsMain
+        val constraintLayoutContactsExpandable: ConstraintLayout = itemView.constraintLayoutContactsExpandable
+    }
+
+}
--- app/src/main/java/hr/atoscvc/salesforcemobile/ContactListActivity.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/ContactListActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_contact_list.*
 
 class ContactListActivity : AppCompatActivity() {
 
@@ -16,6 +18,48 @@ class ContactListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_contact_list)
 
         mAuth = FirebaseAuth.getInstance()
+
+        recyclerViewContacts.setHasFixedSize(true)
+        recyclerViewContacts.layoutManager = LinearLayoutManager(this)
+
+        val contactList = ArrayList<Contact>()
+
+        contactList.add(Contact(
+                "akjssgddad",
+                0,
+                1,
+                "Ana",
+                "Peric",
+                "Atos",
+                "091251244",
+                "email@email.com",
+                "asdjbajhbdsnadjhas asda sda safsd fasdgasdg asdga sdgasdg asdg sdgasd ggasdg asdg asdgas dgasdg sadg sdg asdgg asdg sadg"
+        ))
+        contactList.add(Contact(
+                "tffcvbh",
+                1,
+                0,
+                "Matej",
+                "Bubic",
+                "Siemens",
+                "087654567899",
+                "email@gmail.com",
+                "alksnbdabnsd"
+        ))
+        contactList.add(Contact(
+                "klejwf",
+                1,
+                3,
+                "Tea",
+                "Bubic",
+                "Google",
+                "56456789867",
+                "yahoo@gmail.com",
+                "masjdban asdaisd"
+        ))
+
+        val adapter = ContactAdapter(contactList, applicationContext)
+        recyclerViewContacts.adapter = adapter
     }
 
     override fun onResume() {
