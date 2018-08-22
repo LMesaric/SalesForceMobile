c74e0a29ccedac41dfa140f4ca4f80a7cce33ec7
LMesaric <luka.mesaric@fer.hr>
2018-08-22 09:50:04 +0200

    Contact must always be passed through intent. Added data to other fields.

--- app/src/main/java/hr/atoscvc/salesforcemobile/ContactDetailsActivity.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/ContactDetailsActivity.kt
@@ -17,32 +17,35 @@ class ContactDetailsActivity : AppCompatActivity() {
         const val requestCodeEditContact = 5
     }
 
-    private var contact: Contact? = null
+    private lateinit var contact: Contact
     private var isChanged: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_details)
-        contact = intent.getSerializableExtra(getString(R.string.EXTRA_CONTACT_ENTIRE_OBJECT)) as? Contact
+        contact = intent.getSerializableExtra(getString(R.string.EXTRA_CONTACT_ENTIRE_OBJECT)) as Contact
     }
 
     override fun onResume() {
         super.onResume()
 
-        tvContactDetailsName.text = ContactNameConcatenate.fullName(contact!!, resources, true)
-        tvContactDetailsEmail.text = contact?.email
-        tvContactDetailsPhoneNumber.text = contact?.phone
-        tvContactDetailsDetails.text = contact?.details
+        tvContactDetailsName.text = ContactNameConcatenate.fullName(contact, resources, true)
+        tvContactDetailsCompanyName.text = contact.company?.name
+        tvContactDetailsStatus.text = resources.getStringArray(R.array.status_array)[contact.status]
+        tvContactDetailsEmail.text = contact.email
+        tvContactDetailsPhoneNumber.text = contact.phone
+        tvContactDetailsPrefTime.text = resources.getStringArray(R.array.contactPreferredTime_array)[contact.preferredTime]
+        tvContactDetailsDetails.text = contact.details
     }
 
     fun onContactEmail(@Suppress("UNUSED_PARAMETER") view: View) {
-        val email: String? = contact?.email?.trim()
+        val email: String? = contact.email?.trim()
         if (email.isNullOrBlank()) {
             Toast.makeText(this, getString(R.string.wrongEmail), Toast.LENGTH_SHORT).show()
         } else {
             val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:")).apply {
                 putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-                putExtra(Intent.EXTRA_TEXT, "${getString(R.string.emailGreeting)} ${ContactNameConcatenate.fullName(contact!!, resources, true)}, \n\n")
+                putExtra(Intent.EXTRA_TEXT, "${getString(R.string.emailGreeting)} ${ContactNameConcatenate.fullName(contact, resources, true)}, \n\n")
             }
             try {
                 startActivity(Intent.createChooser(emailIntent, getString(R.string.sendEmailChooser)))
@@ -54,7 +57,7 @@ class ContactDetailsActivity : AppCompatActivity() {
     }
 
     fun onContactCall(@Suppress("UNUSED_PARAMETER") view: View) {
-        val phone: String? = contact?.phone?.trim()
+        val phone: String? = contact.phone?.trim()
         if (phone.isNullOrBlank()) {
             Toast.makeText(this, getString(R.string.wrongPhoneNumber), Toast.LENGTH_SHORT).show()
         } else {
@@ -69,12 +72,12 @@ class ContactDetailsActivity : AppCompatActivity() {
     }
 
     fun onContactText(@Suppress("UNUSED_PARAMETER") view: View) {
-        val phone: String? = contact?.phone?.trim()
+        val phone: String? = contact.phone?.trim()
         if (phone.isNullOrBlank()) {
             Toast.makeText(this, getString(R.string.wrongPhoneNumber), Toast.LENGTH_SHORT).show()
         } else {
             val smsIntent = Intent(Intent.ACTION_VIEW, Uri.parse("smsto:$phone")).apply {
-                putExtra("sms_body", "${getString(R.string.smsGreeting)} ${ContactNameConcatenate.fullName(contact!!, resources, false)}, \n")
+                putExtra("sms_body", "${getString(R.string.smsGreeting)} ${ContactNameConcatenate.fullName(contact, resources, false)}, \n")
             }
             try {
                 startActivity(Intent.createChooser(smsIntent, getString(R.string.sendTextChooser)))
@@ -90,7 +93,7 @@ class ContactDetailsActivity : AppCompatActivity() {
             putExtra(getString(R.string.EXTRA_IS_EDITOR_FOR_NEW_ITEM), false)
             putExtra(getString(R.string.EXTRA_CONTACT_ENTIRE_OBJECT), contact)
         }
-        ContactEditFragment.chosenCompany = contact?.company
+        ContactEditFragment.chosenCompany = contact.company
         startActivityForResult(intent, ContactDetailsActivity.requestCodeEditContact)
         //TODO Testirati radi li implementirani refresh RecycleViewa nakon Savea
     }
@@ -107,7 +110,7 @@ class ContactDetailsActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == ContactDetailsActivity.requestCodeEditContact) {
             if (resultCode == AppCompatActivity.RESULT_OK) {
-                contact = data?.getSerializableExtra(getString(R.string.EXTRA_CONTACT_ENTIRE_OBJECT)) as? Contact
+                contact = data?.getSerializableExtra(getString(R.string.EXTRA_CONTACT_ENTIRE_OBJECT)) as Contact
                 isChanged = true
             }
         }
