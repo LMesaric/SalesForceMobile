be18486b67f4d7d001d9cfe6f9f6dda6c8265fec
LMesaric <luka.mesaric@fer.hr>
2018-08-24 14:02:20 +0200

    Screen rotation no longer resets Fragments.

--- app/src/main/java/hr/atoscvc/salesforcemobile/MainActivity.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/MainActivity.kt
@@ -13,8 +13,9 @@ import com.google.firebase.firestore.FirebaseFirestore
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
-//TODO Prilikom rotacije ekrana uvijek skoci na Home i ostane staro selectano na dnu (na Registration se vraca na dio s imenom i prezimenom)
 //TODO MainActivity napraviti kao tabbedActivity ?
+
+//FIXME SVAKI PUT KAD CREATEAMO FRAGMENT ON OSTAJE SAVEAN, KAO I ONAJ STARI (MEMORIJA RASTE DO 370 MB) - REUSE https://stackoverflow.com/questions/19219458/fragment-on-screen-rotation
 
 class MainActivity : AppCompatActivity(), LogoutListener, ContactAdapter.RecyclerViewContactsOnClickListener, CompanyAdapter.RecyclerViewCompaniesOnClickListener {
 
@@ -152,14 +153,19 @@ class MainActivity : AppCompatActivity(), LogoutListener, ContactAdapter.Recycle
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if (item != null) {
-            if (item.itemId == R.id.action_logout) {
-                sendToLogin()
-            } else if (item.itemId == R.id.action_account_settings) {
-                val intent = Intent(this, ChangePasswordActivity::class.java)
-                startActivity(intent)
+
+            when (item.itemId) {
+
+                R.id.action_logout -> {
+                    sendToLogin()
+                }
+
+                R.id.action_account_settings -> {
+                    val intent = Intent(this, ChangePasswordActivity::class.java)
+                    startActivity(intent)
+                }
             }
         }
-
         return true
     }
 
