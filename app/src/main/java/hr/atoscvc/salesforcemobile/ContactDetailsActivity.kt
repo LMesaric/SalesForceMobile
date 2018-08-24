16886e8f535e54160387fd879b71aae8f8c265d0
LMesaric <luka.mesaric@fer.hr>
2018-08-24 12:34:02 +0200

    Non-mandatory contact details are not shown as blank.

--- app/src/main/java/hr/atoscvc/salesforcemobile/ContactDetailsActivity.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/ContactDetailsActivity.kt
@@ -32,10 +32,44 @@ class ContactDetailsActivity : AppCompatActivity() {
         tvContactDetailsName.text = ContactNameConcatenate.fullName(contact, resources, true)
         tvContactDetailsCompanyName.text = contact.company?.name
         tvContactDetailsStatus.text = resources.getStringArray(R.array.status_array)[contact.status]
-        tvContactDetailsEmail.text = contact.email
-        tvContactDetailsPhoneNumber.text = contact.phone
-        tvContactDetailsPrefTime.text = resources.getStringArray(R.array.contactPreferredTime_array)[contact.preferredTime]
-        tvContactDetailsDetails.text = contact.details
+
+        val email: String? = contact.email
+        if (email.isNullOrBlank()) {
+            tvContactDetailsEmail.visibility = View.GONE
+            fabContactDetailsSendEmail.hide()
+        } else {
+            tvContactDetailsEmail.text = contact.email
+            tvContactDetailsEmail.visibility = View.VISIBLE
+            fabContactDetailsSendEmail.show()
+        }
+
+        val phone: String? = contact.phone
+        if (phone.isNullOrBlank()) {
+            tvContactDetailsPhoneNumber.visibility = View.GONE
+            fabContactDetailsCall.hide()
+            fabContactDetailsSendText.hide()
+        } else {
+            tvContactDetailsPhoneNumber.text = contact.phone
+            tvContactDetailsPhoneNumber.visibility = View.VISIBLE
+            fabContactDetailsCall.show()
+            fabContactDetailsSendText.show()
+        }
+
+        val prefTimeIndex: Int = contact.preferredTime
+        if (prefTimeIndex == 0) {
+            tvContactDetailsPrefTime.visibility = View.GONE
+        } else {
+            tvContactDetailsPrefTime.text = resources.getStringArray(R.array.contactPreferredTime_array)[prefTimeIndex]
+            tvContactDetailsPrefTime.visibility = View.VISIBLE
+        }
+
+        val details: String? = contact.details
+        if (details.isNullOrBlank()) {
+            tvContactDetailsDetails.visibility = View.GONE
+        } else {
+            tvContactDetailsDetails.text = contact.details
+            tvContactDetailsDetails.visibility = View.VISIBLE
+        }
 
         val shouldHideButtons: Boolean = intent.getBooleanExtra(getString(R.string.EXTRA_CONTACT_HIDE_EDIT_BUTTONS), false)
         if (shouldHideButtons) {
@@ -121,6 +155,7 @@ class ContactDetailsActivity : AppCompatActivity() {
         if (requestCode == ContactDetailsActivity.requestCodeEditContact) {
             if (resultCode == AppCompatActivity.RESULT_OK) {
                 contact = data?.getSerializableExtra(getString(R.string.EXTRA_CONTACT_ENTIRE_OBJECT)) as Contact
+                intent.putExtra(getString(R.string.EXTRA_CONTACT_ENTIRE_OBJECT), contact)
                 isChanged = true
             }
         }
