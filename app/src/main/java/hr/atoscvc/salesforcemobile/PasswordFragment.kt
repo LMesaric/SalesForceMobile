a8e2b55c93b6d3199f0727984ee5b2c2f08b1cc1
LMesaric <luka.mesaric@fer.hr>
2018-08-28 14:59:24 +0200

    Specified type as not nullable.

--- app/src/main/java/hr/atoscvc/salesforcemobile/PasswordFragment.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/PasswordFragment.kt
@@ -22,7 +22,7 @@ class PasswordFragment : Fragment(), View.OnClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        val view = inflater.inflate(R.layout.fragment_password, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_password, container, false)
         view.btnFinishPass.setOnClickListener(this)
 
         db = FirebaseFirestore.getInstance()
