f9ec1083778511b2045c6fe4d38ea8681d696d00
LMesaric <luka.mesaric@fer.hr>
2018-09-05 19:52:54 +0200

    Textwatcher now generates a new vector drawable only when it is different from the old one.

--- app/src/main/java/hr/atoscvc/salesforcemobile/DrawableTextWatcher.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/DrawableTextWatcher.kt
@@ -13,15 +13,27 @@ class DrawableTextWatcher(
         private val editTextFirst: EditText,
         private val editTextSecond: EditText? = null
 ) : TextWatcher {
-    override fun afterTextChanged(s: Editable?) {}
 
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+    private var iconTextBefore: String? = null
+    private var iconTextAfter: String? = null
 
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    private fun createIconText(): String {
         val letterOne: Char? = if (editTextFirst.text.isBlank()) null else editTextFirst.text.trim()[0].toUpperCase()
         val letterTwo: Char? = if (editTextSecond == null || editTextSecond.text.isBlank()) null else editTextSecond.text.trim()[0].toUpperCase()
-        val iconText = "${letterOne ?: ""}${letterTwo ?: ""}"
-        val drawable: TextDrawable = TextDrawable.builder().buildRound(iconText, ColorGenerator.MATERIAL.getColor(key))
-        imageView.setImageDrawable(drawable)
+        return "${letterOne ?: ""}${letterTwo ?: ""}"
+    }
+
+    override fun afterTextChanged(s: Editable?) {}
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        iconTextBefore = createIconText()
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        iconTextAfter = createIconText()
+
+        if (iconTextBefore != iconTextAfter) {
+            imageView.setImageDrawable(TextDrawable.builder().buildRound(iconTextAfter, ColorGenerator.MATERIAL.getColor(key)))
+        }
     }
 }
